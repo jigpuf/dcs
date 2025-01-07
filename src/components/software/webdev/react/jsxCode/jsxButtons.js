7b9c975a47bd5612e6c1d04c34dd97a031304fb1
jigpuf <32025384+jigpuf@users.noreply.github.com>
2025-01-07 01:20:22 -0600

    Update jsxButtons.js

--- src/components/software/webdev/react/jsxCode/jsxButtons.js
+++ src/components/software/webdev/react/jsxCode/jsxButtons.js
@@ -1,12 +1,81 @@
 import React from "react";
 const JSXButtons = () => {
   const buttons = [
-    { step: "Top Level copmponent", code: [""], notes: [""], display: "" },
-    { step: "Buttons component", code: [""], notes: [""], display: "" },
-    { step: "Render button", code: [""], notes: [""], display: "" },
-    { step: "CSS", code: [""], notes: [""], display: "" },
-    { step: "", code: [""], notes: [""], display: "" },
+    {
+      step: "Buttons component",
+      code: [
+        "import React from 'react';",
+        "import ButtonMaker from '../../utilities/buttonMaker';",
+        "import Fuses from '../panels/fuses';",
+        "import Breaker from './circuitProtection/breaker';",
+        "const CircuitProtection = () => {",
+        "const items = [",
+        "{ name: 'Fuses', slug: 'fuses', component: <Fuses /> },",
+        "{ name: 'Breakers', slug: 'breakers', component: <Breaker /> },",
+        "];",
+        "return (",
+        "<div>",
+        "<ButtonMaker buttons={items} />",
+        "</div>",
+        ");",
+        "};",
+        "export default CircuitProtection;",
+      ],
+      notes: [""],
+      display: "",
+    },
+    {
+      step: "Render button",
+      code: [
+        "import React, {useState} from 'react';",
+        "const ButtonMaker = (props) =>{",
+        "const [buttonState, setButtonState] = useState(undefined);",
+        "const buttons = props.buttons.map(item =>{",
+        "return (",
+        "<button ",
+        "key={item.slug}",
+        "onClick={() => setButtonState(item.slug)}",
+        "className={buttonState === item.slug ? 'tier1 active': 'teir1'}",
+        ">{item.name}",
+        "</button>)",
+        "});",
+        "const activeButton = props.buttons.filter(item =>{",
+        "return buttonState ===item.slug;",
+        "})",
+        "const renderActiveButton = activeButton[0] && activeButton[0].component;",
+        "return(",
+        "<div>",
+        "<ul>",
+        "{buttons}",
+        "</ul>",
+        "<hr />",
+        "{renderActiveButton}",
+        "</div>",
+        ");",
+        "}",
+        "export default ButtonMaker;",
+        "",
+      ],
+      notes: [""],
+      display: "",
+    },
+    {
+      step: "CSS",
+      code: [
+        ".active {",
+        "background-color: lightblue;",
+        "}",
+        "button {",
+        "border-radius: 10px;",
+        "color: black;",
+        "}",
+        "",
+      ],
+      notes: [""],
+      display: "",
+    },
   ];
+
   const renderButtons = buttons.map((item) => {
     const notes = item.notes.map((note) => {
       return <li>{note}</li>;
@@ -18,7 +87,10 @@ const JSXButtons = () => {
       <tr>
         <td>{item.step}</td>
         <td>
-          <ol>{codes}</ol>
+          <details>
+            <summary>Code</summary>
+            <ol>{codes}</ol>
+          </details>
         </td>
         <td>
           <ol>{notes}</ol>
